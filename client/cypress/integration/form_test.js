3e048e0c8998d584e036cd296d07e20099c56f82
MicahJank <MicahJank@users.noreply.github.com>
2020-03-26 14:00:07 -0500

    added some comments

--- client/cypress/integration/form_test.js
+++ client/cypress/integration/form_test.js
@@ -4,18 +4,22 @@ describe('Testing the Form.js component', () => {
     })
 
     it('Adds texts to the forms and submits it', () => {
+        // Get the Name input and type a name in it.
         cy.get('input[name="name"]')
           .type('Micah')
           .should('have.value', 'Micah');
 
+          //Get the Email input and type an email address in it
         cy.get('input[name="email"]')
           .type('micahjank@testing.com')
           .should('have.value', 'micahjank@testing.com');
         
+          //Get the password input and type a password in it
         cy.get('input[name="password"]')
           .type('secretpassword')
           .should('have.value', 'secretpassword');
 
+          // Set up a test that will check to see if a user can check the terms of service box
         cy.get('input[type="checkbox"]')
           .check()
           .should('be.checked');
@@ -24,10 +28,12 @@ describe('Testing the Form.js component', () => {
           .select('thing1')
           .should('have.value', 'thing1');
 
+          // Check to see if a user can submit the form data
         cy.get('button[type="submit"]')
           .click();
     })
 
+    // Check for form validation if an input is left empty
     it('Gives an error message if a field is left blank', () => {
         // clear is needed because the error message doesnt show unless something
         // is typed and then cleared
